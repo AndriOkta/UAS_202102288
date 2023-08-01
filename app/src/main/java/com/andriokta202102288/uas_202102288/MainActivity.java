1ef150b9e2139571a56ceb6d3eb9194172f06857
AndriOkta <andreroger167@gmail.com>
2023-08-01 18:29:15 +0700

    layout.main

---
+++ app/src/main/java/com/andriokta202102288/uas_202102288/MainActivity.java
@@ -0,0 +1,14 @@
+package com.andriokta202102288.uas_202102288;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
